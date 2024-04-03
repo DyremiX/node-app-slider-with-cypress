fa10b721d2ae92103b02524ab5c226c7d060d1d5
Michał_Dubiel <michal@dubiel.pl>
2024-04-03 21:36:36 +0200

    xd

--- public/script.js
+++ public/script.js
@@ -13,3 +13,12 @@ const swiper = new Swiper(".swiper", {
         onlyInViewport: false
     }
 }); 
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3930942,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
