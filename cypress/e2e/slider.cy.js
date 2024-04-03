f40bca2852db5ed43bcd481a3977b358929cf2aa
Michał_Dubiel <michal@dubiel.pl>
2024-04-03 21:17:19 +0200

    Heheszki

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,14 @@
 describe('Swiper Gallery Test', function () {
+  it('Checks if second slide contains "Rome"', function () {
+    cy.visit('http://localhost:3000');
+    // cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,7 +16,7 @@ describe('Swiper Gallery Test', function () {
 
 describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
@@ -16,3 +24,33 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    // cy.screenshot()
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if second slide contains "Rome"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+// describe('Swiper Gallery Test', function () {
+//   it('Checks if second slide contains "Rome"', function () {
+//     cy.visit('http://app:3000');
+//     cy.get('.swiper-button-next').click();
+//     cy.get('.swiper-slide-active').should('contain', 'Rome');
+//   });
+// });
